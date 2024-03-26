27a5b2bab4d21751394fb59f3b9d2fea49535dca
DanaeD1867 <116097166+DanaeD1867@users.noreply.github.com>
2024-03-26 18:00:46 -0400

    import timeit, create add test

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import timeit
 #32-bit integer operation benchmark
+
+#10^10 additions
+def add_test_int(): 
+    result = 0 
+    return result
 #64-bit floating point operation benchmark
 #Memory benchmark
 #Hard drive benchmark 1
