2a71e209bda55d27c535f0423f7cc2d1721138a1
DanaeD1867 <116097166+DanaeD1867@users.noreply.github.com>
2024-03-26 18:08:41 -0400

    create mult int test

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def add_test_int():
         result += 5
     return result
 
+#10^9 mulitplication
+def mult_test_int(): 
+    result = 1
+    for _ in range(5*10**9): 
+        result *= 2
+    return result
+
 if __name__ == '__main__': 
     add_int_time = timeit.timeit(add_test_int)
     print(add_int_time)
